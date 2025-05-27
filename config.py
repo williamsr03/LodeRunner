26085d85e09a1d76d0a852f4399095f03f83606c
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-05-27 14:28:44 -0500

    Changed config_level to hopefully take json file

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os, csv
+import json
 
 
 class Config:
-    LEVEL_WIDTH = 35
-    LEVEL_HEIGHT = 21
+    LEVEL_WIDTH = 32
+    LEVEL_HEIGHT = 22
 
     CELL_SIZE = 24
     WINDOW_WIDTH = CELL_SIZE*LEVEL_WIDTH
@@ -12,14 +13,22 @@ class Config:
     hidden_flag = False
 
     @staticmethod
-    def config_level(num):
-        with open(os.path.join('levels', 'level{}.csv').format(num)) as file_data:
-            row_num = 0
-            for row in csv.reader(file_data):
-                Config.LEVEL_WIDTH = len(row)
-                row_num += 1
-            Config.LEVEL_HEIGHT = row_num
+    def config_level(source, level_index=0):
+        if source.endswith('.json'):
+            with open(source, 'r') as f:
+                levels = json.load(f)
+                # Use the first level in the JSON file as an example
+                scene = levels[0]['scene']
+                Config.LEVEL_HEIGHT = len(scene)
+                Config.LEVEL_WIDTH = max(len(row) for row in scene) if scene else 0
+        else:
+            with open(source) as file_data:
+                row_num = 0
+                for row in csv.reader(file_data):
+                    Config.LEVEL_WIDTH = len(row)
+                    row_num += 1
+                Config.LEVEL_HEIGHT = row_num
 
-        Config.WINDOW_WIDTH = Config.CELL_SIZE*Config.LEVEL_WIDTH
-        Config.WINDOW_HEIGHT = Config.CELL_SIZE*Config.LEVEL_HEIGHT
+        Config.WINDOW_WIDTH = Config.CELL_SIZE * Config.LEVEL_WIDTH
+        Config.WINDOW_HEIGHT = Config.CELL_SIZE * Config.LEVEL_HEIGHT
         Config.hidden_flag = False
