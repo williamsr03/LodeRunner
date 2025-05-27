84dd1d5e1ed4c5cca32453528b7fb7aa3e174a89
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-05-27 14:28:10 -0500

    Needed for library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LodeRunner',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Reid Williams',
+    description='LodeRunner game as a Python package',
+)
