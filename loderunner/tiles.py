b93b62b7d6ef76e1520eddfd28f75ec2b1f7bbfb
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-06-06 10:59:16 -0500

    Changed to lower case

--- loderunner/tiles.py
+++ loderunner/tiles.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from loderunner.drawable import Drawable
-from LodeRunner import util
+from loderunner import util
 
 class Tile(Drawable):
     level = []
