7aa4ea8ac18ad3b5d0b0842cb8912994bb0efed1
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-07-02 14:55:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[],
+    include_package_data=True,
     author='Reid Williams',
     description='LodeRunner game as a Python package',
 )
