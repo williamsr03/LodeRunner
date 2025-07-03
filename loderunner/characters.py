1469d71df0f8e8a2d25db8735120598cbbfee2af
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-07-03 15:40:46 -0500

    Update characters.py

--- loderunner/characters.py
+++ loderunner/characters.py
@@ -98,7 +98,10 @@ class Character (Drawable):
         next_pos = (self._x, self._y+1)
 
         if self._y+1 < Config.LEVEL_HEIGHT:
-            if not Tile.query(next_pos, 'standable') and not Tile.query(self.pos(), 'grabbable') and not baddie.pos() == tile.coord:
+            if not Tile.query(next_pos, 'standable') and not Tile.query(self.pos(), 'grabbable'):
+                for baddie in Baddie.baddies:
+                    if baddie.pos() == tile.coor():
+                        return
                 self.apply_move(0, 1)
                 # Schedule the next fall if still in the air
                 self.schedule_fall(frames=3)  # Adjust frames for fall speed
