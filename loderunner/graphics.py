517f1c39a751b0560e0c44ced5d2efff994f146e
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-07-09 16:21:24 -0500

    Update graphics.py

--- loderunner/graphics.py
+++ loderunner/graphics.py
@@ -21,7 +21,7 @@ simple example, here is a complete program to draw a circle of radius
 10 centered in a 100x100 window:
 
 --------------------------------------------------------------------
-from graphics import *
+from graphics_images import *
 
 def main():
     win = GraphWin("My Circle", 100, 100)
