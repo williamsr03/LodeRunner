0d9fee7f955b186947412fd5c50d5288a4f006d7
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-07-11 12:15:17 -0500

    Update graphics.py

--- loderunner/graphics.py
+++ loderunner/graphics.py
@@ -315,8 +315,7 @@ class GraphWin(tk.Canvas):
     def checkKey(self):
         """Return last key pressed or None if no key pressed since last call"""
         if self.isClosed():
-            #raise GraphicsError("checkKey in closed window")
-            return
+            raise GraphicsError("checkKey in closed window")
         self.update()
         key = self.lastKey
         self.lastKey = ""
